63a2262b16a5e50ad4092ee8cb5ffb5e2a5b46d6
Yang He <yyhe@g.clemson.edu>
2016-09-29 12:16:07 -0400

    split the weka functions

---
+++ src/wekaFunctions.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.meta.FilteredClassifier;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.filters.unsupervised.attribute.Remove;
+
+public class wekaFunctions {
+	public static FilteredClassifier train(Instances train) throws Exception
+	{
+		//train.setClassIndex(classIndex);
+		
+		Remove rm = new Remove();
+		rm.setAttributeIndices("1");  // REMOVING ID ATTRIBUTE AS THAT WON'T BE INPUT TO THE CLASSIFIER
+		// classifier
+		J48 j48 = new J48();
+		//j48.setUnpruned(true);        // using an unpruned J48
+		// meta-classifier
+		FilteredClassifier fc = new FilteredClassifier();
+		fc.setFilter(rm);
+		fc.setClassifier(j48);
+		// train
+		fc.buildClassifier(train);
+		return fc;
+		
+	}
+	
+	public static double eval(FilteredClassifier fc, Instances train, Instances test)  throws Exception
+	{
+		Evaluation eval = new Evaluation(train);
+		eval.evaluateModel(fc, test);
+		return eval.pctCorrect();
+	}
+
+	public static double evalCrossValidation(FilteredClassifier fc, Instances data) throws Exception
+	{
+		Random random = new Random();
+		Evaluation eval = new Evaluation(data);
+		eval.crossValidateModel(fc, data, 10, random);
+		return eval.pctCorrect();
+	}
+}
