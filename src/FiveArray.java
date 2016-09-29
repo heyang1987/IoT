4b998943294419f68bd7a1115d5401e9245fdc6b
Yang He <yyhe@g.clemson.edu>
2016-09-29 16:28:48 -0400

    test

--- src/FiveArray.java
+++ src/FiveArray.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class FiveArray {
 	
 	public static void main(String[] args) throws Exception {
-		ThreeArray test = new ThreeArray();
+		//ThreeArray test = new ThreeArray();
 		ArrayList<Integer> userArray = new ArrayList<Integer>();  
 		userArray = getUserID.getUserIDArray();
-		test.run(userArray);
+		ThreeArray.run(userArray);
 	}
 	
 }
